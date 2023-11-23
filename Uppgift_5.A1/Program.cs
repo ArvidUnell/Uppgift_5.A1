d9295e75c1508337376dbc489ddec7521256d12b
arun06001 <arun06001@E-PW0234KE>
2023-11-23 10:19:37 +0100

    Add project files.

---
+++ Uppgift_5.A1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Uppgift_5_A1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meningar = { "Hakuna matata.", "Missilen vet var den är under alla omständigheter","Jag är en astronaut" };
+
+            foreach (string mening in meningar)
+            {
+                Console.WriteLine(mening + "\n");
+            }
+
+            Console.WriteLine("Tryck på valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
